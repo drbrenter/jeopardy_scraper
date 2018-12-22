afc3765c555a807fc92204249b406b28df841ad1
drbrenter <sendbrentsomemail@gmail.com>
2018-12-22 11:51:59 -0800

    First commit of data structures, parsing routines, and requirements file

---
+++ jeopardy.py
@@ -0,0 +1,9 @@
+
+class JeopardyRound:
+    def __init__(self):
+        """Data structure representing a round of single or double jeopardy"""
+
+
+class JeopardyEpisode:
+    def __init__(self):
+        """Python data structure representing an entire three round episode of Jeopardy"""
