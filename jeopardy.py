644e867ab251e4eeba7d64ca8d860b5aec211e13
drbrenter <sendbrentsomemail@gmail.com>
2019-01-06 18:15:37 -0800

    Added functionality to view remaining questions in a round in a user friendly manner

--- jeopardy.py
+++ jeopardy.py
@@ -8,8 +8,10 @@ class JeopardyRound:
         self._num_questions  = 5
         if rnum == 1:
             self._round_name = 'Jeopardy'
+            self._values = [200, 400, 600, 800, 1000]
         elif rnum == 2:
             self._round_name = 'Double Jeopardy'
+            self._values = [400, 800, 1200, 1600, 2000]
         elif rnum == 3:
             self._round_name = 'Final Jeopardy'
             self._num_categories = 1
@@ -34,7 +36,7 @@ class JeopardyRound:
 
 class JeopardyEpisode:
     def __init__(self):
-        """Python data structure representing an entire three round episode of Jeopardy"""
+        """Python data structure representing an entire three-round episode of Jeopardy"""
 
         # Date when episode first aired
         self._air_date = None;
@@ -56,3 +58,34 @@ def clear_screen():
     """Clear previous outputs from console. Should work cross-platform."""
     os.system('cls' if os.name=='nt' else 'clear')
 
+
+def get_clue_idx(category_number, question_number, questions_per_category):
+    """Helper function to get the index of a particular clue in the list"""
+    return question_number + category_number*questions_per_category
+
+
+def get_game_board(round_struct):
+    """Reads the data structure (dictionary array) for the jeopardy round and returns
+       a string containing the remaining clues to choose from."""
+    game_board_text = ''
+
+    # Iterate through categories
+    for category_idx, category in enumerate(round_struct._categories):
+        category_text = str(category_idx) + ': ' + category + ': '
+
+        # Iterate through questions within category
+        for question_idx in range(round_struct._num_questions):
+            # Get index of clue
+            idx = get_clue_idx(category_idx, question_idx, round_struct._num_questions)
+
+            # Add a user friendly printout if this clue is valid
+            question_text = str(question_idx) + ': '
+            if round_struct._clues[idx]['isvalid']:
+                question_text += '$' + str(round_struct._values[question_idx]) + ', '
+            else:
+                question_text += '   '
+            category_text += question_text
+        category_text += '\n'
+        game_board_text += category_text
+
+    return game_board_text
